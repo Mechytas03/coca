a433e7fbf17b5abf83eca5a1e193ee5c08af0046
Mechytas03 <96792291+Mechytas03@users.noreply.github.com>
2022-05-22 10:44:42 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="pag en construccion">
+    <meta name="keywords"content ="estudiar , css, html, codo a codo,henry">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>practicando</title>
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./estilo.css">
+    <link rel="shortcut icon" href="img/favicon.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2? display=swap" rel="stylesheet"> 
+    
+</head>
+<body>
+    <header>
+        <div class="container">
+            <a href="index.html">
+                <img src="img/logo.svg" alt="404" class="logo">
+            </a>
+            <nav>
+                <a href="#inicio">INICIO</a>
+                <a href="#nosotros">NOSOTROS</a>
+                <a href="#servicios">SERVICIOS</a>
+                <a href="#galeria">GALERIA</a>
+                <a href="#contactenos">CONTACTO</a>
+            </nav>
+            <a href="#" class="hamb">
+                <i class="fa-solid fa-bars"></i>
+            </a>
+
+        </div>
+    </header>
+    <main>
+        <section id="inicio">
+            <img src="./img/bannerprincipal.jpg" alt="">
+            <div class="bloque-inicio">
+                <h1>BIENVENIDOS A MIS PRACTICAS</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Cupiditate fugiat impedit cum porro, eligendi placeat voluptatum alias? 
+                    Error omnis ipsa consectetur ipsum consequuntur nihil distinctio amet dolorum, commodi eos aspernatur.
+                </p>
+                <a href="#nosotros" class="boton" >VER MAS</a>
+                
+            </div>
+        </section>
+        <section id="nosotros" class="seccion">
+            <div class="container"><p> Lorem ipsum dolor sit amet consectetur adipisicing elit.Magni perferendis velit asperiores rerum, mollitia alias commodi quo dolor, saepe officia beatae dignissimos voluptatibus molestiae est sapiente nemo impedit earum recusandae.
+                </p>
+                </div>
+        </section>
+        <section id="servicios" >
+            <div class="container">
+                <div class="row">
+
+                    <div class="columna columna-33 columna-mobile-50 columna-mobile-100 gallery" >
+                        <div class="bloque-servicio ">
+                            <div class="bloque-img-servicio ">
+                                <img src="./img/servicio1.jpg" alt="ups">
+                            </div>
+                            <div class="bloque-contenido-servicio">
+                                <h3>Servicio 1</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Ratione possimus rerum deleniti ipsa quibusdam qui beatae nisi alias nihil eveniet suscipit aperiam, 
+                                    error adipisci libero minima sit saepe cupiditate quas.</p>
+                                    <a href="#" class="boton boton-blanco">VER MAS</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="columna columna-33 columna-mobile-100 gallery">
+                        <div class="container">
+                            <div class="row">
+                                <div class="columna columna-33 columna-mobile-50 columna-mobile-100 gallery">
+                                    <div class="bloque-servicio ">
+                                        <div class="bloque-img-servicio ">
+                                            <img src="./img/servicio2.jpg" alt="ups">
+                                        </div>
+                                        <div class="bloque-contenido-servicio">
+                                            <h3>Servicio 2</h3>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Ratione possimus rerum deleniti ipsa quibusdam qui beatae nisi alias nihil eveniet suscipit aperiam, 
+                                                error adipisci libero minima sit saepe cupiditate quas.</p>
+                                                <a href="#" class="boton boton-blanco">VER MAS</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="columna columna-33 columna-mobile-50 columna-mobile-100 gallery">
+                        <div class="container">
+                            <div class="row">
+                                <div class="columna columna-33 columna-mobile-50 columna-mobile-100 gallery">
+                                    <div class="bloque-servicio">
+                                        <div class="bloque-img-servicio ">
+                                            <img src="./img/servicio3.jpg" alt="ups">
+                                        </div>
+                                        <div class="bloque-contenido-servicio">
+                                            <h3>Servicio 3</h3>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Ratione possimus rerum deleniti ipsa quibusdam qui beatae nisi alias nihil eveniet suscipit aperiam, 
+                                                error adipisci libero minima sit saepe cupiditate quas.</p>
+                                                <a href="#" class="boton boton-blanco">VER MAS</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        </section>
+
+        
+        <section id="galeria">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class=" columna-33" >
+                        <div class="cuadrado-perfecto galleria">
+                            <img src="./img/servicio1.jpg" alt="">
+                            <h4>IMAGEN 1</h4>
+                        </div> 
+                    </div>
+                    <div class=" columna-33" >
+                        <div class="cuadrado-perfecto ">
+                            <img src="./img/servicio2.jpg" alt="">
+                            <h4>IMAGEN 2</h4>
+                        </div> 
+                         </div>
+
+                    <div class="  columna-33" >
+                        <div class="cuadrado-perfecto">
+                            <img src="./img/servicio3.jpg" alt="">
+                            <h4>IMAGEN 3</h4>
+                        </div>  
+
+                    </div>
+
+                    <div class="  columna-33" >
+                        <div class="cuadrado-perfecto">
+                            <img src="./img/servicio4.jpg" alt="">
+                            <h4>IMAGEN 4</h4>
+                        </div> 
+                         </div>
+
+                    <div class="  columna-33" >
+                        <div class="cuadrado-perfecto">
+                            <img src="./img/servicio5.jpg" alt="">
+                            <h4>IMAGEN 5</h4>
+                        </div> 
+                         </div>
+
+                    <div class="  columna-33" > 
+                        <div class="cuadrado-perfecto">
+                            <img src="./img/servicio6.jpg" alt="">
+                            <h4>IMAGEN 6</h4>
+                        </div> 
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <section id="contactenos" class="seccion">
+                        <iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Buenos%20Aires+(nu%C3%B1ez)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="columna columna-41  comluna-mobile-100 empujar-58 empujar-0 sinpadding-mobile" >
+                        <form action="index.php" method="post">
+                            <div class="form-block">
+                                <input type="text" name="nombre" class="form-contol" placeholder="Nombre">
+                            </div>
+                            <div class="form-block">
+                                <input type="email" name="email" class="form-contol  " placeholder="email">
+                            </div>
+                            <div class="form-block">
+                                <textarea name="mensaje" ></textarea>
+                            </div>
+                            <div class="form-block bloque-ultimo" aria-placeholder="Mensaje">
+                                    <input type="submit" class="boton boton-negro" value="ENVIAR">
+                            </div>
+                            <?php
+                                if($_SERVER["REQUEST_METHOO"] == "POST"){
+                                    $nombre = $_POST["nombre"];
+                                    $email = $_POST["email"];
+                                    $mensaje = $_POST["mensaje"];
+                                    if(isset($nombre)){
+                                        if(isset($email){
+                                            if(isset($mensaje)){
+                                                $para = "nuevocorreo@gmail.com";
+                                                $asunto = "ESTO ES UNA PRUEBA";
+                                                $cuerpo = $nombre."\n".$email."\n".$mensaje;
+                                                $adicional = "from :noreplay@merceddes.com";
+                                                mail($para, $asunto, $cuerpo,$adicional) ;
+                                                ?>
+                                                <p>ENVIO EXITOSO</p>
+                                        <?php
+                                            }
+                                        })
+
+                                    }
+
+                                }
+                            ?>
+                        </form>
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        
+    </main>
+    <footer class="seccion">
+<div class="container">
+    <div class="row">
+        <div class="columna columna-25 columna-mobile-100">
+            <img src="./img/logo-blanco.png" alt="" class="logo-footer">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam ipsa quia accusamus iste possimus, consectetur autem repudiandae numquam eaque assumenda esse tempore ipsum vel ad atque molestias reprehenderit molestiae! Perspiciatis.</p>
+        </div>
+        <div class="columna columna-25 columna-mobile-100">
+            <h3>
+                TEMAS RELACIONADOS
+            </h3>
+
+                <ul>
+                    <li><a href="#">TEMA1</a></li>
+                    <li><a href="#">TEMA2</a></li>
+                    <li><a href="#">TEMA3</a></li>
+                </ul>
+        </div>
+        <div class="columna columna-25 columna-mobile-100">
+            <h3>
+DATOS DE CONTACTO            </h3>
+
+                <ul>
+                    <li>mechys2007@gmail.com</li>
+                    <li>1138405640</a></li>
+                    <li>Buenos Aires,Argentina <br>Nuñez</li>
+                </ul>
+        </div>
+        <div class="columna columna-25 columna-mobile-100">
+            <h3>
+REDES SOCIALES
+            </h3>
+
+                <ul class="redes">
+                    <li><a href="#">
+                        <i class="fa-brands fa-facebook-square"></i>
+                    </a></li>
+                    <li><a href="#">
+                        <i class="fa-brands fa-twitter-square"></i>
+                    </a></li>
+                    <li><a href="#">
+                        <i class="fa-brands fa-instagram-square"></i>
+                    </a>
+                        </li>
+                </ul>
+        </div>
+
+
+    </div>
+</div>
+<div class="barra-footer">
+    &copy;DERECHOS E IZQUIERDOS RESERVADOS
+</div>
+    </footer>
+    <script src="./js/jquery.js"></script>
+    <script src="./js//funcione.js"></script>
+
+
+</body>
+</html>
